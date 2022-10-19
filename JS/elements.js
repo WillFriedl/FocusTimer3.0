458d352bbb3ba5b14b9e63c9e3eeaea05af8e176
William Friedl <110947625+WillFriedl@users.noreply.github.com>
2022-10-18 22:59:57 -0300

    Add files via upload

---
+++ JS/elements.js
@@ -0,0 +1,59 @@
+const switchTheme = document.querySelector('.timerTheme')
+const switchColorTimer = document.querySelector('.player')
+const buttonLightMode = document.querySelector('.buttonLightmode')
+const buttonDarkMode = document.querySelector('.buttonDarkmode')
+const buttonForest = document.querySelector('.forest')
+const buttonRain = document.querySelector('.rain')
+const buttonCoffeshop = document.querySelector('.coffeshop')
+const buttonFireplace = document.querySelector('.fireplace')
+const buttonForestVolume = document.querySelector('.forest-volume')
+const buttonRainVolume = document.querySelector('.rain-volume')
+const buttonCoffeshopVolume = document.querySelector('.coffeshop-volume')
+const buttonFireplaceVolume = document.querySelector('.fireplace-volume')
+const svgPlay = document.querySelector('.play .svgPlayer')
+const svgPlayFake = document.querySelector('.playFake .svgPlayer')
+const svgStop = document.querySelector('.stop .svgPlayer')
+const svgTimerUp = document.querySelector('.up .svgPlayer')
+const svgTimerDown = document.querySelector('.down .svgPlayer')
+const svgForest = document.querySelector('.forest .svg')
+const svgRain = document.querySelector('.rain .svg')
+const svgCoffeshop = document.querySelector('.coffeshop .svg')
+const svgFireplace = document.querySelector('.fireplace .svg')
+const minutesDisplay = document.querySelector('.minutes')
+const secondsDisplay = document.querySelector('.seconds')
+const buttonPlay = document.querySelector('.play')
+const buttonPlayFake = document.querySelector('.playFake')
+const buttonStop = document.querySelector('.stop')
+const buttonTimerUp = document.querySelector('.up')
+const buttonTimerDown = document.querySelector('.down')
+
+export {
+    switchTheme,
+    switchColorTimer,
+    buttonLightMode,
+    buttonDarkMode,
+    buttonForest,
+    buttonRain,
+    buttonCoffeshop,
+    buttonFireplace,
+    buttonForestVolume,
+    buttonRainVolume,
+    buttonCoffeshopVolume,
+    buttonFireplaceVolume,
+    svgPlay,
+    svgPlayFake,
+    svgStop,
+    svgTimerUp,
+    svgTimerDown,
+    svgForest,
+    svgRain,
+    svgCoffeshop,
+    svgFireplace,
+    minutesDisplay,
+    secondsDisplay,
+    buttonPlay,
+    buttonPlayFake,
+    buttonStop,
+    buttonTimerUp,
+    buttonTimerDown
+}
