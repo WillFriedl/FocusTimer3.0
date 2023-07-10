cefe76eb9ed531731506b93a995f3a8deba2bb21
William Friedl <110947625+WillFriedl@users.noreply.github.com>
2023-07-09 21:44:59 -0300

    adjust timer synchronization

--- JS/timer.js
+++ JS/timer.js
@@ -1,90 +1,130 @@
 import Sounds from "./sounds.js"
-import {
-    buttonTimerUp,
-    buttonTimerDown,
-} from "./elements.js"
+import { buttonTimerUp, buttonTimerDown } from "./elements.js"
 
 export default function Timer({
-    minutesDisplay,
-    secondsDisplay,
-    controls,
+  minutesDisplay,
+  secondsDisplay,
+  controls
 }) {
+  let timerTimeout
+  let minutes = Number(minutesDisplay.textContent)
+  let endTime
+  let minutesDefault
 
-    let timerTimeOut
-    let minutes = Number(minutesDisplay.textContent)
+  function updateTimerDisplay(minutes, seconds) {
+    minutesDisplay.textContent = String(minutes).padStart(2, "0")
+    secondsDisplay.textContent = String(seconds).padStart(2, "0")
+  }
 
-    function updateTimerDisplay(minutes, seconds) {
-        minutesDisplay.textContent = String(minutes).padStart(2, "0")
-        secondsDisplay.textContent = String(seconds).padStart(2, "0")
+  function resetTimer() {
+    updateTimerDisplay(25, 0)
+    clearTimeout(timerTimeout)
+    controls.resetControls()
+  }
+
+  function countdown() {
+      console.time("timer")
+    const startTime = Date.now()
+    endTime = startTime + 25 * 60 * 1000 // 25 minutes in milliseconds
+
+    function updateRemainingTime() {
+      const currentTime = Date.now()
+      const remainingTime = endTime - currentTime
+
+      if (remainingTime <= 0) {
+        resetTimer()
+        Sounds().kitchenTimer.play()
+        return
       }
 
-      function resetTimer() {
-        updateTimerDisplay(25, 0)
-        clearTimeout(timerTimeOut)
-        controls.resetControls()
-    }
+      minutesDefault = Math.floor(remainingTime / 60000)
+      let remainingSeconds = Math.ceil((remainingTime / 1000) % 60)
 
-    function countdown() {
-        timerTimeOut = setTimeout(function() {
-            let minutes = Number(minutesDisplay.textContent)
-            let seconds = Number(secondsDisplay.textContent)
-      
-        updateTimerDisplay(25, 0)
-      
-        if (minutes <= 0 && seconds <= 0) {
-            resetTimer()
-            Sounds().kitchenTimer.play()
-            return
-        }
-    
-        if( seconds <= 0 ) {
-            seconds = 60
-            --minutes
-        }
-
-        updateTimerDisplay(minutes, String(seconds - 1))
-    
-        countdown()
-    }, 1000)
-    }
-
-    buttonTimerUp.addEventListener('click', function() {
-        let newMinutes = Number(minutesDisplay.textContent) + 5
-        let newSeconds = Number(secondsDisplay.textContent)
-
-        if(minutesDisplay.textContent >= 55) {
-            newMinutes = 60
-            newSeconds = 0
-            updateTimerDisplay(newMinutes, newSeconds)
-        }
-
-        minutes = newMinutes
-        updateTimerDisplay(minutes, secondsDisplay.textContent)
-    })
-
-    buttonTimerDown.addEventListener('click', function() {
-        let newMinutes = Number(minutesDisplay.textContent) - 5
-        let newSeconds = Number(secondsDisplay.textContent)
-
-        if(minutesDisplay.textContent < 5) {
-            newMinutes = 25
-            newSeconds = 0
-            updateTimerDisplay(newMinutes, newSeconds)
-            resetTimer()
-        }
-
-        minutes = newMinutes
-        updateTimerDisplay(minutes, secondsDisplay.textContent)
-    })
-
-    function updateMinutes(newMinutes) {
-        minutes = newMinutes
-    }
-
-      return {
-        updateTimerDisplay,
-        resetTimer,
-        countdown,
-        updateMinutes,
+      if (remainingSeconds === 60) {
+        remainingSeconds = 0
+        minutesDefault++
       }
+
+      if (minutesDefault < 0) {
+        minutesDefault = 0
+      }
+
+      if (minutesDefault === 25 && remainingSeconds === 0) {
+        minutesDefault = 25
+        remainingSeconds = 0
+      }
+
+      updateTimerDisplay(minutesDefault, remainingSeconds)
+
+      const elapsedMilliseconds = currentTime - startTime
+      const delay = 1000 - (elapsedMilliseconds % 1000) // Adjust to sync with seconds
+
+
+      const timeDiff = delay - 1000 // Time in milliseconds of difference every 1 second
+      console.log(timeDiff)
+
+      timerTimeout = setTimeout(updateRemainingTime, delay)
+    }
+
+    updateRemainingTime()
+    console.timeEnd("timer")
+  }
+
+  buttonTimerUp.addEventListener("click", function () {
+    let newMinutes = Number(minutesDisplay.textContent) + 5
+    let newSeconds = Number(secondsDisplay.textContent)
+
+    if (minutesDisplay.textContent >= 55) {
+        return 
+    }
+
+    minutes = newMinutes
+
+    if (endTime) {
+      const currentTime = Date.now()
+      const remainingTimeMillis = endTime - currentTime
+      const newRemainingTimeMillis = remainingTimeMillis + 5 * 60 * 1000
+      const newEndTime = currentTime + newRemainingTimeMillis
+      endTime = newEndTime
+      minutesDefault = Math.floor(newRemainingTimeMillis / 60000)
+      updateTimerDisplay(minutesDefault, newSeconds)
+    } else {
+      updateTimerDisplay(newMinutes, newSeconds)
+    }
+  })
+
+  buttonTimerDown.addEventListener("click", function () {
+    let newMinutes = Number(minutesDisplay.textContent) - 5
+    let newSeconds = Number(secondsDisplay.textContent)
+
+    if (minutesDisplay.textContent < 5) {
+      newMinutes = 25
+      newSeconds = 0
+      updateTimerDisplay(newMinutes, newSeconds)
+      resetTimer()
+      const currentTime = Date.now()
+      endTime = currentTime + newMinutes * 60 * 1000
+      minutesDefault = newMinutes
+    } else {
+      minutes = newMinutes
+      updateTimerDisplay(minutes, newSeconds)
+      const currentTime = Date.now()
+      const remainingTimeMillis = endTime - currentTime
+      const newRemainingTimeMillis = remainingTimeMillis - 5 * 60 * 1000
+      const newEndTime = currentTime + newRemainingTimeMillis
+      endTime = newEndTime
+      minutesDefault = Math.floor(newRemainingTimeMillis / 60000)
+    }
+  })
+
+  function updateMinutes(newMinutes) {
+    minutes = newMinutes
+  }
+
+  return {
+    updateTimerDisplay,
+    resetTimer,
+    countdown,
+    updateMinutes
+  }
 }
